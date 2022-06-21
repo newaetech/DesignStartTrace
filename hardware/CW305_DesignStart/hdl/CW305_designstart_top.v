0a6d877ea45653c476bfa3893e566e1f570f7c3b
jpcrypt <jpthibault@newae.com>
2022-06-21 15:11:02 -0400

    More Husky/TraceWhisperer under-the-hood updates.

--- hardware/CW305_DesignStart/hdl/CW305_designstart_top.v
+++ hardware/CW305_DesignStart/hdl/CW305_designstart_top.v
@@ -109,7 +109,6 @@ module CW305_designstart_top #(
   wire trigger_clk_psincdec;
   wire trigger_clk_psdone;
 
-  wire arm;
   wire capturing;
   wire fe_clk;
 
@@ -131,7 +130,7 @@ module CW305_designstart_top #(
   end
 
   assign led1 = count[22];              // clock alive
-  assign led2 = arm;
+  assign led2 = arm_usb;
   assign led3 = capturing;
 
   assign trig_out = soft_trig_passthru? m3_trig_out : trace_trig_out;
@@ -335,8 +334,8 @@ module CW305_designstart_top #(
    wire         fifo_wr;
    wire         fifo_read;
    wire         fifo_flush;
-   wire         reg_arm;
-   wire         reg_arm_feclk;
+   wire         arm_usb;
+   wire         arm_fe;
    wire         clear_errors;
    wire [17:0]  fifo_out_data;
    wire [5:0]   fifo_status;
@@ -387,6 +386,7 @@ module CW305_designstart_top #(
       .usb_clk          (clk_usb_buf),
       .reset_pin        (reset_pin),
       .fpga_reset       (fpga_reset),
+      .I_external_arm   (1'b0), // Husky only
       .I_fe_clock_count (count),
       .buildtime        (buildtime),
       .O_trace_en       (), // Husky only
@@ -421,7 +421,6 @@ module CW305_designstart_top #(
       .reg_write        (reg_write), 
       .reg_addrvalid    (reg_addrvalid),
 
-      .arm              (arm),
       .capturing        (capturing),
 
       // FIFO interface:
@@ -431,8 +430,8 @@ module CW305_designstart_top #(
       .fifo_wr          (fifo_wr),
       .fifo_read        (fifo_read),
       .fifo_flush       (fifo_flush),
-      .reg_arm          (reg_arm),
-      .reg_arm_feclk    (reg_arm_feclk),
+      .arm_usb          (arm_usb),
+      .arm_fe           (arm_fe),
       .clear_errors     (clear_errors),
       .fifo_out_data    (fifo_out_data),
       .fifo_status      (fifo_status),
@@ -469,8 +468,8 @@ module CW305_designstart_top #(
 
       .I_fifo_read              (fifo_read),
       .I_fifo_flush             (fifo_flush),
-      .I_clear_read_flags       (reg_arm),
-      .I_clear_write_flags      (reg_arm_feclk),
+      .I_clear_read_flags       (arm_usb),
+      .I_clear_write_flags      (arm_fe),
       .I_clear_errors           (clear_errors),
 
       .O_data                   (fifo_out_data),
